d94bf855c6c91cfdb1d267c2e897e40e1db37ea0
namornejk <kudrnatomas000@gmail.com>
2020-08-18 04:46:37 +0200

    Intro and about me created.

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var introContainerHeight = $('#hello-box').height();
+
+$(window).scroll(function() {
+
+    var wScroll = $(this).scrollTop();
+
+    if (wScroll <= introContainerHeight + 200) {
+
+        $('#headline').css({
+            'transform': 'translate(0px, ' + wScroll / 2 + '%)',
+            'display': 'initial'
+        });
+
+        $('#welcome').css({
+            'transform': 'translate(0px, -' + wScroll / 2 + '%)'
+        });
+
+    } else {
+        $('#headline').css({
+            'display': 'none'
+        });
+    }
+
+    $('#about-photo').css({
+        'transform': 'translate(0px, -' + wScroll / 10 + '%)'
+    });
+})
